08b4a261027a2654ff091733f07afd65d3c78bf1
Elmer Alay <alayelmer1993@gmail.com>
2019-08-07 16:41:19 -0600

    se actualizó el script en base a la última versión del modelo

--- bd/script_bd_sis.sql
+++ bd/script_bd_sis.sql
@@ -1,4 +1,3 @@
-
 -- -----------------------------------------------------
 -- sis_db
 -- -----------------------------------------------------
@@ -9,107 +8,113 @@ USE sis_db;
 -- categoria
 -- -----------------------------------------------------
 CREATE TABLE categoria (
-  idcategoria INT NOT NULL AUTO_INCREMENT,
+  id_categoria INT NOT NULL AUTO_INCREMENT,
   nombre VARCHAR(45) NULL,
-  PRIMARY KEY (idcategoria)
-);
-
--- -----------------------------------------------------
--- persona
--- -----------------------------------------------------
-CREATE TABLE persona (
-  dpi VARCHAR(25) NOT NULL,
-  nombre VARCHAR(45) NULL,
-  apellido VARCHAR(45) NULL,
-  direccion VARCHAR(45) NULL,
-  telefono VARCHAR(8) NULL,
-  email VARCHAR(45) NULL,
-  PRIMARY KEY (dpi),
-  UNIQUE INDEX email_UNIQUE (email)
-);
-
--- -----------------------------------------------------
--- usuario
--- -----------------------------------------------------
-CREATE TABLE usuario (
-  dpi VARCHAR(25) NOT NULL,
-  password VARCHAR(45) NULL,
-  PRIMARY KEY (dpi),
-  CONSTRAINT fk_Usuario_persona1
-    FOREIGN KEY (dpi)
-    REFERENCES persona (dpi)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+  PRIMARY KEY (id_categoria)
 );
 
 -- -----------------------------------------------------
 -- servicio
 -- -----------------------------------------------------
 CREATE TABLE servicio (
-  idservicio INT NOT NULL AUTO_INCREMENT,
+  id_servicio INT NOT NULL AUTO_INCREMENT,
   nombre_servicio VARCHAR(45) NULL,
-  idcategoria INT NOT NULL,
-  PRIMARY KEY (idservicio),
-  INDEX fk_servicio_categoria1_idx (idcategoria),
+  id_categoria INT NOT NULL,
+  PRIMARY KEY (id_servicio),
+  INDEX fk_servicio_categoria1_idx (id_categoria),
   CONSTRAINT fk_servicio_categoria1
-    FOREIGN KEY (idcategoria)
-    REFERENCES categoria (idcategoria)
+    FOREIGN KEY (id_categoria)
+    REFERENCES categoria (id_categoria)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- municipio
+-- -----------------------------------------------------
+CREATE TABLE municipio (
+  id_municipio INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(45) NULL,
+  PRIMARY KEY (id_municipio)
 );
 
 -- -----------------------------------------------------
 -- empresa
 -- -----------------------------------------------------
 CREATE TABLE empresa (
-  idempresa INT NOT NULL AUTO_INCREMENT,
+  id_empresa INT NOT NULL AUTO_INCREMENT,
+  nit INT NULL,
   nombre VARCHAR(45) NULL,
   direccion VARCHAR(45) NULL,
-  PRIMARY KEY (idempresa)
+  telefono VARCHAR(12) NULL,
+  zona INT NULL,
+  latitud DOUBLE NULL,
+  longitud DOUBLE NULL,
+  id_municipio INT NOT NULL,
+  email VARCHAR(100) NULL,
+  password VARCHAR(100) NULL,
+  PRIMARY KEY (id_empresa),
+  INDEX fk_empresa_municipio1_idx (id_municipio),
+  UNIQUE INDEX email_UNIQUE (email),
+  UNIQUE INDEX nit_UNIQUE (nit),
+  CONSTRAINT fk_empresa_municipio1
+    FOREIGN KEY (id_municipio)
+    REFERENCES municipio (id_municipio)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
 );
 
 -- -----------------------------------------------------
 -- lista_servicio
 -- -----------------------------------------------------
 CREATE TABLE lista_servicio (
-  idempresa INT NOT NULL,
-  idservicio INT NOT NULL,
+  id_empresa INT NOT NULL,
+  id_servicio INT NOT NULL,
   precio DOUBLE NULL,
-  INDEX fk_lista_servicio_servicio1_idx (idservicio),
-  PRIMARY KEY (idempresa, idservicio),
-  INDEX fk_lista_servicio_Empresa1_idx (idempresa),
+  INDEX fk_lista_servicio_servicio1_idx (id_servicio),
+  PRIMARY KEY (id_empresa, id_servicio),
+  INDEX fk_lista_servicio_Empresa1_idx (id_empresa),
   CONSTRAINT fk_lista_servicio_servicio1
-    FOREIGN KEY (idservicio)
-    REFERENCES servicio (idservicio)
+    FOREIGN KEY (id_servicio)
+    REFERENCES servicio (id_servicio)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_lista_servicio_Empresa1
-    FOREIGN KEY (idempresa)
-    REFERENCES empresa (idempresa)
+    FOREIGN KEY (id_empresa)
+    REFERENCES empresa (id_empresa)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- forma_pago
+-- -----------------------------------------------------
+CREATE TABLE forma_pago (
+  id_pago INT NOT NULL AUTO_INCREMENT,
+  forma VARCHAR(45) NULL,
+  PRIMARY KEY (id_pago)
 );
 
 -- -----------------------------------------------------
 -- detalle_contrato
 -- -----------------------------------------------------
 CREATE TABLE detalle_contrato (
-  idcontrato INT NOT NULL AUTO_INCREMENT,
+  id_contrato INT NOT NULL AUTO_INCREMENT,
   fecha DATETIME NULL,
-  dpi VARCHAR(25) NOT NULL,
-  idempresa INT NOT NULL,
-  idservicio INT NOT NULL,
-  INDEX fk_detalle_contrato_Usuario1_idx (dpi),
-  PRIMARY KEY (idcontrato),
-  INDEX fk_detalle_contrato_lista_servicio1_idx (idempresa, idservicio),
-  CONSTRAINT fk_detalle_contrato_Usuario1
-    FOREIGN KEY (dpi)
-    REFERENCES usuario (dpi)
+  id_empresa INT NOT NULL,
+  id_servicio INT NOT NULL,
+  id_pago INT NOT NULL,
+  PRIMARY KEY (id_contrato),
+  INDEX fk_detalle_contrato_lista_servicio1_idx (id_empresa, id_servicio),
+  INDEX fk_detalle_contrato_forma_pago1_idx (id_pago),
+  CONSTRAINT fk_detalle_contrato_lista_servicio1
+    FOREIGN KEY (id_empresa , id_servicio)
+    REFERENCES lista_servicio (id_empresa, id_servicio)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT fk_detalle_contrato_lista_servicio1
-    FOREIGN KEY (idempresa , idservicio)
-    REFERENCES lista_servicio (idempresa , idservicio)
+  CONSTRAINT fk_detalle_contrato_forma_pago1
+    FOREIGN KEY (id_pago)
+    REFERENCES forma_pago (id_pago)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
@@ -118,14 +123,14 @@ CREATE TABLE detalle_contrato (
 -- comentario
 -- -----------------------------------------------------
 CREATE TABLE comentario (
-  idcomentario INT NOT NULL AUTO_INCREMENT,
+  id_comentario INT NOT NULL AUTO_INCREMENT,
   comentario VARCHAR(250) NULL,
-  idcontrato INT NOT NULL,
-  PRIMARY KEY (idcomentario),
-  INDEX fk_comentario_detalle_contrato1_idx (idcontrato),
+  id_contrato INT NOT NULL,
+  PRIMARY KEY (id_comentario),
+  INDEX fk_comentario_detalle_contrato1_idx (id_contrato),
   CONSTRAINT fk_comentario_detalle_contrato1
-    FOREIGN KEY (idcontrato)
-    REFERENCES detalle_contrato (idcontrato)
+    FOREIGN KEY (id_contrato)
+    REFERENCES detalle_contrato (id_contrato)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
@@ -134,14 +139,38 @@ CREATE TABLE comentario (
 -- calificacion
 -- -----------------------------------------------------
 CREATE TABLE calificacion (
-  idcalificacion INT NOT NULL AUTO_INCREMENT,
+  id_calificacion INT NOT NULL AUTO_INCREMENT,
   calificacion INT NULL,
-  idcontrato INT NOT NULL,
-  PRIMARY KEY (idcalificacion),
-  INDEX fk_calificacion_detalle_contrato1_idx (idcontrato),
+  id_contrato INT NOT NULL,
+  PRIMARY KEY (id_calificacion),
+  INDEX fk_calificacion_detalle_contrato1_idx (id_contrato),
   CONSTRAINT fk_calificacion_detalle_contrato1
-    FOREIGN KEY (idcontrato)
-    REFERENCES detalle_contrato (idcontrato)
+    FOREIGN KEY (id_contrato)
+    REFERENCES detalle_contrato (id_contrato)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- usuario
+-- -----------------------------------------------------
+CREATE TABLE usuario (
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NULL,
+  dpi VARCHAR(25) NULL,
+  nombre VARCHAR(45) NULL,
+  apellido VARCHAR(45) NULL,
+  direccion VARCHAR(45) NULL,
+  telefono VARCHAR(8) NULL,
+  zona INT NULL,
+  latitud DOUBLE NULL,
+  longitud DOUBLE NULL,
+  id_municipio INT NOT NULL,
+  INDEX fk_persona_municipio1_idx (id_municipio),
+  PRIMARY KEY (email),
+  CONSTRAINT fk_persona_municipio1
+    FOREIGN KEY (id_municipio)
+    REFERENCES municipio (id_municipio)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
@@ -150,19 +179,20 @@ CREATE TABLE calificacion (
 -- trabajador
 -- -----------------------------------------------------
 CREATE TABLE trabajador (
-  idtrabajador INT NOT NULL AUTO_INCREMENT,
-  dpi VARCHAR(25) NOT NULL,
-  idempresa INT NOT NULL,
-  PRIMARY KEY (idtrabajador),
-  INDEX fk_trabajador_Empresa1_idx (idempresa),
-  CONSTRAINT fk_trabajador_persona1
-    FOREIGN KEY (dpi)
-    REFERENCES persona (dpi)
+  id_empleado INT NOT NULL AUTO_INCREMENT,
+  id_empresa INT NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id_empleado),
+  INDEX fk_trabajador_Empresa1_idx (id_empresa),
+  INDEX fk_trabajador_persona1_idx (email),
+  CONSTRAINT fk_trabajador_Empresa1
+    FOREIGN KEY (id_empresa)
+    REFERENCES empresa (id_empresa)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT fk_trabajador_Empresa1
-    FOREIGN KEY (idempresa)
-    REFERENCES empresa (idempresa)
+  CONSTRAINT fk_trabajador_persona1
+    FOREIGN KEY (email)
+    REFERENCES usuario (email)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
@@ -171,14 +201,14 @@ CREATE TABLE trabajador (
 -- foto
 -- -----------------------------------------------------
 CREATE TABLE foto (
-  idfoto INT NOT NULL AUTO_INCREMENT,
-  ruta VARCHAR(45) NULL,
-  dpi VARCHAR(25) NOT NULL,
-  PRIMARY KEY (idfoto),
-  INDEX fk_foto_persona1_idx (dpi ASC) VISIBLE,
-  CONSTRAINT fk_foto_persona1
-    FOREIGN KEY (dpi)
-    REFERENCES persona (dpi)
+  id_foto INT NOT NULL AUTO_INCREMENT,
+  ruta VARCHAR(250) NULL,
+  id_empleado INT NOT NULL,
+  PRIMARY KEY (id_foto),
+  INDEX fk_foto_trabajador1_idx (id_empleado),
+  CONSTRAINT fk_foto_trabajador1
+    FOREIGN KEY (id_empleado)
+    REFERENCES trabajador (id_empleado)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
@@ -187,14 +217,14 @@ CREATE TABLE foto (
 -- video
 -- -----------------------------------------------------
 CREATE TABLE video (
-  idvideo INT NOT NULL AUTO_INCREMENT,
-  ruta_video VARCHAR(45) NULL,
-  dpi VARCHAR(25) NOT NULL,
-  PRIMARY KEY (idvideo),
-  INDEX fk_video_persona1_idx (dpi ASC) VISIBLE,
-  CONSTRAINT fk_video_persona1
-    FOREIGN KEY (dpi)
-    REFERENCES persona (dpi)
+  id_video INT NOT NULL AUTO_INCREMENT,
+  ruta_video VARCHAR(250) NULL,
+  id_empleado INT NOT NULL,
+  PRIMARY KEY (id_video),
+  INDEX fk_video_trabajador1_idx (id_empleado),
+  CONSTRAINT fk_video_trabajador1
+    FOREIGN KEY (id_empleado)
+    REFERENCES trabajador (id_empleado)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
@@ -203,14 +233,31 @@ CREATE TABLE video (
 -- documento
 -- -----------------------------------------------------
 CREATE TABLE documento (
-  iddocumento INT NOT NULL AUTO_INCREMENT,
-  documento VARCHAR(45) NULL,
-  dpi VARCHAR(25) NOT NULL,
-  PRIMARY KEY (iddocumento),
-  INDEX fk_documento_persona1_idx (dpi),
-  CONSTRAINT fk_documento_persona1
-    FOREIGN KEY (dpi)
-    REFERENCES persona (dpi)
+  id_documento INT NOT NULL AUTO_INCREMENT,
+  documento VARCHAR(250) NULL,
+  id_empleado INT NOT NULL,
+  PRIMARY KEY (id_documento),
+  INDEX fk_documento_trabajador1_idx (id_empleado),
+  CONSTRAINT fk_documento_trabajador1
+    FOREIGN KEY (id_empleado)
+    REFERENCES trabajador (id_empleado)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+-- -----------------------------------------------------
+-- suscripcion
+-- -----------------------------------------------------
+CREATE TABLE suscripcion (
+  id_suscripcion INT NOT NULL AUTO_INCREMENT,
+  total VARCHAR(45) NULL,
+  fecha DATE NULL,
+  id_empresa INT NOT NULL,
+  PRIMARY KEY (id_suscripcion),
+  INDEX fk_subscripcion_empresa1_idx (id_empresa),
+  CONSTRAINT fk_subscripcion_empresa1
+    FOREIGN KEY (id_empresa)
+    REFERENCES empresa (id_empresa)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
